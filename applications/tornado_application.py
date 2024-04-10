b8616c452be8ce2a14b36afb8847305d53a691e3
jschaff <jschaff@scripps.edu>
2024-04-10 12:24:17 -0700

    Finalize the tornado application launcher

--- applications/tornado_application.py
+++ applications/tornado_application.py
@@ -18,13 +18,14 @@ an interface for launching the web server
 
 """
 
-import json
 from pathlib import Path
 from pprint import pformat
 from pydoc import locate
 from types import SimpleNamespace
 from typing import Union, Optional
+import importlib
 import inspect
+import json
 import logging
 import os
 import sys
@@ -43,148 +44,6 @@ from biothings.web.settings import configs
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-
-
-class ServerApplication(tornado.web.Application):
-    """
-    https://www.tornadoweb.org/en/stable/web.html#application-configuration
-    Main entrypoint for generating the tornado web server application.
-
-    Constructor has the following signature with parameter definition
-    explainations at the above link:
-
-    class tornado.web.Application(
-        handlers: Optional[List[Union[Rule, Tuple]]] = None,
-        default_host: Optional[str] = None,
-        transforms: Optional[List[Type[OutputTransform]]] = None,
-        **settings
-    )
-    """
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-
-def use_curl():
-    """
-    Use curl implementation for tornado http clients.
-    More on https://www.tornadoweb.org/en/stable/httpclient.html
-    """
-    tornado.httpclient.AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient")
-
-
-def get_handlers(biothings, addons=None):
-    """
-    Generates the tornado.web.Application `(regex, handler_class, options) tuples
-    <http://www.tornadoweb.org/en/stable/web.html#application-configuration>`_.
-
-    Configuration Parameters:
-    > metadata
-    > APP_LIST
-    > APP_PREFIX
-    > APP_VERSION
-    """
-    handlers = {}
-    addons = addons or []
-    for rule in biothings.config.APP_LIST + addons:
-        pattern = rule[0]
-        handler = load_class(rule[1])
-        setting = rule[2] if len(rule) == 3 else {}
-        assert handler, rule[1]
-
-        if "{typ}" in pattern or "{tps}" in pattern:
-            if not issubclass(handler, BaseQueryHandler):
-                raise TypeError("Not a biothing_type-aware handler.")
-            if "{tps}" in pattern and len(biothings.metadata.types) <= 1:
-                continue  # '{tps}' routes only valid for multi-type apps
-            for biothing_type in biothings.metadata.types:
-                _pattern = pattern.format(
-                    pre=biothings.config.APP_PREFIX,
-                    ver=biothings.config.APP_VERSION,
-                    typ=biothing_type,
-                    tps=biothing_type,
-                ).replace("//", "/")
-                _setting = dict(setting)
-                _setting["biothing_type"] = biothing_type
-                handlers[_pattern] = (_pattern, handler, _setting)
-        elif "{pre}" in pattern or "{ver}" in pattern:
-            pattern = pattern.format(pre=biothings.config.APP_PREFIX, ver=biothings.config.APP_VERSION).replace(
-                "//", "/"
-            )
-            if "()" not in pattern:
-                handlers[pattern] = (pattern, handler, setting)
-        else:  # no pattern translation
-            handlers[pattern] = (pattern, handler, setting)
-
-    handlers = list(handlers.values())
-    logger.info("API Handlers:\n%s", pformat(handlers, width=200))
-    return handlers
-
-
-def get_app(cls, config, settings=None, handlers=None):
-    """
-    Return the tornado.web.Application defined by this config.
-    **Additional** settings and handlers are accepted as parameters.
-    """
-    if isinstance(config, configs.ConfigModule):
-        biothings = BiothingsNamespace(config)
-        _handlers = BiothingsAPI._get_handlers(biothings, handlers)
-        _settings = BiothingsAPI._get_settings(biothings, settings)
-        app = cls(_handlers, **_settings)
-        app.biothings = biothings
-        app._populate_optionsets(config, _handlers)
-        app._populate_handlers(_handlers)
-        return app
-    if isinstance(config, configs.ConfigPackage):
-        biothings = BiothingsNamespace(config.root)
-        _handlers = [(f"/{c.APP_PREFIX}/.*", cls.get_app(c, settings)) for c in config.modules]
-        _settings = BiothingsAPI._get_settings(biothings, settings)
-        app = cls(_handlers + handlers or [], **_settings)
-        app.biothings = biothings
-        # app._populate_optionsets(config, handlers or [])
-        # app._populate_handlers(handlers or [])
-        app._populate_optionsets(config, _handlers + handlers or [])
-        app._populate_handlers(_handlers + handlers or [])
-        return app
-    raise TypeError("Invalid config type. Must be a ConfigModule or ConfigPackage.")
-
-
-class TornadoAPILauncher:
-    # tornado uses its own event loop which is
-    # a wrapper around the asyncio event loop
-
-    def __init__(self, config=None):
-        # About debug mode in tornado:
-        # https://www.tornadoweb.org/en/stable/guide/running.html \
-        # #debug-mode-and-automatic-reloading
-        super().__init__(config)
-        self.handlers = []  # additional handlers
-        self.host = None
-
-    @staticmethod
-    def use_curl():
-        """ """
-        tornado.httpclient.AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient")
-
-    def get_app(self):
-        return BiothingsAPI.get_app(self.config, self.settings, self.handlers)
-
-    def get_server(self):
-        # Use case example:
-        # Run API in an external event loop.
-        app = self.get_app()
-        logger.info("All Handlers:\n%s", pformat(app.biothings.handlers, width=200))
-        return tornado.httpserver.HTTPServer(app, xheaders=True)
-
-    def start(self, port=8000):
-        self._configure_logging()
-
-        http_server = self.get_server()
-        http_server.listen(port, self.host)
-
-        logger.info('Server is running on "%s:%s"...', self.host or "0.0.0.0", port)
-        loop = tornado.ioloop.IOLoop.instance()
-        loop.start()
 
 
 def load_configuration(configuration_file: Optional[Union[str, Path]] = None) -> dict:
@@ -227,9 +86,10 @@ def load_configuration(configuration_file: Optional[Union[str, Path]] = None) ->
     else:
         application_file = Path(__file__).resolve().absolute()
         application_directory = Path(application_file).parent
+        configuration_directory = Path(application_directory).joinpath("configuration")
 
         configuration_filename = "tornado.json"
-        configuration_file = application_directory.joinpath(configuration_filename)
+        configuration_file = configuration_directory.joinpath(configuration_filename)
 
     try:
         with open(str(configuration_file), "r", encoding="utf-8") as file_handle:
@@ -241,50 +101,67 @@ def load_configuration(configuration_file: Optional[Union[str, Path]] = None) ->
     return tornado_configuration
 
 
+class ServerApplication(tornado.web.Application):
+    """
+    https://www.tornadoweb.org/en/stable/web.html#application-configuration
+    Main entrypoint for generating the tornado web server application.
+
+    Constructor has the following signature with parameter definition
+    explainations at the above link:
+
+    class tornado.web.Application(
+        handlers: Optional[List[Union[Rule, Tuple]]] = None,
+        default_host: Optional[str] = None,
+        transforms: Optional[List[Type[OutputTransform]]] = None,
+        **settings
+    )
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
 def get_application(configuration: dict) -> tornado.web.Application:
     """
     Takes the loaded configuration and generates an instance of
     ServerApplication, which inherits from tornado.web.Application
     """
+    application_settings = configuration["application"]["settings"]
+    application_handlers = configuration["application"]["handlers"]
+    handlers = []
+    for handler_mapping in application_handlers:
+        try:
+            handler_package = importlib.import_module(handler_mapping["package"])
+            handler_instance = getattr(handler_package, handler_mapping["handler"])
+            handler_regex = handler_mapping["regex"]
+            handlers.append((handler_regex, handler_instance))
+        except Exception as gen_exc:
+            logger.exception(gen_exc)
+
+    application = tornado.web.Application(handlers=handlers, **application_settings)
+    return application
 
 
-def main(app_handlers=None, app_settings=None, use_curl=False):
+def main():
     """
     Start a Biothings API Server
     """
-    app_handlers = app_handlers or []
-    app_settings = app_settings or {}
-
-    launcher = TornadoAPILauncher(options.conf)
-
-    try:
-        if app_settings:
-            launcher.settings.update(app_settings)
-        if app_handlers:
-            launcher.handlers = app_handlers
-        if use_curl:
-            launcher.use_curl()
-
-        launcher.host = options.address
-        launcher.settings.update(debug=options.debug)
-        launcher.settings.update(autoreload=options.autoreload)
-    except Exception:
-        pass
-
     tornado_configuration = load_configuration()
     logger.info("global tornado configuration:\n %s", json.dumps(tornado_configuration, indent=4))
     tornado_application = get_application(tornado_configuration)
+    logger.info("generated tornado application: %s", tornado_application)
 
-    app = BiothingsAPI.get_app(self.config, self.settings, self.handlers)
-    logger.info("All Handlers:\n%s", pformat(app.biothings.handlers, width=200))
-    http_server = tornado.httpserver.HTTPServer(app, xheaders=True)
-    port = 8000
-    host = "0.0.0.0"
-    http_server.listen(port, self.host)
-
-    logger.info('Server is running on "%s:%s"...', self.host or "0.0.0.0", port)
-    loop = tornado.ioloop.IOLoop.instance()
-    loop.start()
+    try:
+        http_server = tornado.httpserver.HTTPServer(tornado_application, xheaders=True)
+        tornado_host = tornado_configuration["network"]["host"]
+        tornado_port = tornado_configuration["network"]["port"]
+        http_server.listen(port=tornado_port, address=tornado_host)
+        logger.info("HTTP server listening @ %s:%s", tornado_host, tornado_port)
+        loop = tornado.ioloop.IOLoop.instance()
+        loop.start()
+    except Exception as gen_exc:
+        logger.exception(gen_exc)
+        raise gen_exc
 
 
 if __name__ == "__main__":
