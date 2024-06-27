1d695a747993296ba44f1a83e6db1afdeb4aef71
ctrl-schaff <59380780+ctrl-schaff@users.noreply.github.com>
2024-06-27 10:07:03 -0700

    Add a README file and simplify the cli entrypoint (#4)
    Co-authored-by: jschaff <jschaff@scripps.edu>

--- biothings_annotator/__init__.py
+++ biothings_annotator/__init__.py
@@ -1,5 +1,5 @@
 from .annotator import Annotator
-from .application import sanic, tornado
+from .application import sanic
 from .biolink import BIOLINK_PREFIX_to_BioThings
 from .exceptions import TRAPIInputError, InvalidCurieError
 from .transformer import ResponseTransformer
