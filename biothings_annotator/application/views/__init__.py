f234bc037b58314c562bf1d6ba189f28c6f336d7
jschaff <jschaff@scripps.edu>
2024-07-24 14:26:12 -0700

    Correct our command-line bugs and the route naming

--- biothings_annotator/application/views/__init__.py
+++ biothings_annotator/application/views/__init__.py
@@ -12,9 +12,13 @@ def build_routes() -> list[dict]:
     For the moment however, we're only targetting adding the handler
     and uri argument for simplicity
     """
-    curie_route = {"handler": CurieView.as_view(), "uri": r"/<curie:str>"}
-    curie_route_mirror = {"handler": CurieView.as_view(), "uri": r"/annotator/<curie:str>"}
-    trapi_route = {"handler": TrapiView.as_view(), "uri": "/"}
-    trapi_route_mirror = {"handler": TrapiView.as_view(), "uri": "/annotator/"}
+    curie_route = {"handler": CurieView.as_view(), "uri": r"/<curie:str>", "name": "curie_endpoint"}
+    curie_route_mirror = {
+        "handler": CurieView.as_view(),
+        "uri": r"/annotator/<curie:str>",
+        "name": "curie_endpoint_mirror",
+    }
+    trapi_route = {"handler": TrapiView.as_view(), "uri": "/", "name": "trapi_endpoint"}
+    trapi_route_mirror = {"handler": TrapiView.as_view(), "uri": "/annotator/", "name": "trapi_endpoint_mirror"}
     route_collection = [curie_route, curie_route_mirror, trapi_route, trapi_route_mirror]
     return route_collection
