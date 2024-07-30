e235432990999a5d96e4faef4a1bc304c5e5bc94
jschaff <jschaff@scripps.edu>
2024-07-30 11:07:22 -0700

    Update the compression test to remove score comparison

--- tests/test_compression.py
+++ tests/test_compression.py
@@ -13,7 +13,7 @@ def test_get_compression(test_annotator: sanic.Sanic):
 
     """
     curie_id = "NCBIGene:1017"
-    endpoint_path = f"/{curie_id}"
+    endpoint_path = f"/{curie_id}?include_extra=0"
     empty_compression_headers = {"Accept-Encoding": ""}
     uncompressed_request, uncompressed_response = test_annotator.test_client.request(
         endpoint_path, http_method="get", headers=empty_compression_headers
@@ -25,7 +25,13 @@ def test_get_compression(test_annotator: sanic.Sanic):
         endpoint_path, http_method="get", headers=compressed_headers
     )
     assert compressed_request.headers["Accept-Encoding"] == compressed_headers["Accept-Encoding"]
-    assert compressed_response.json == uncompressed_response.json
+
+    compressed_body = compressed_response.json
+    compressed_body[curie_id][0].pop("_score")
+
+    uncompressed_body = uncompressed_response.json
+    uncompressed_body[curie_id][0].pop("_score")
+    assert compressed_body == uncompressed_body
 
     assert compressed_response.num_bytes_downloaded < uncompressed_response.num_bytes_downloaded
 
@@ -40,7 +46,7 @@ def test_post_compression(test_annotator: sanic.Sanic, trapi_request: dict):
     compressed   response size : 135222  bytes
     uncompressed response size : 3075763 bytes
     """
-    url = "/"
+    url = "/?include_extra=0"
     empty_compression_headers = {"Accept-Encoding": ""}
     uncompressed_request, uncompressed_response = test_annotator.test_client.request(
         url, http_method="post", json=trapi_request, headers=empty_compression_headers
@@ -52,6 +58,26 @@ def test_post_compression(test_annotator: sanic.Sanic, trapi_request: dict):
         url, http_method="post", json=trapi_request, headers=compressed_headers
     )
     assert compressed_request.headers["Accept-Encoding"] == compressed_headers["Accept-Encoding"]
-    assert compressed_response.json == uncompressed_response.json
+    # {'values_changed': {"root['NCBIGene:284217']['attributes'][0]['value'][0]['_score']": {'new_value': 26.179539, 'old_value': 26.179554}}}
+
+    compressed_body = compressed_response.json
+    uncompressed_body = uncompressed_response.json
+
+    compressed_keys = set(compressed_body.keys())
+    uncompressed_keys = set(uncompressed_body.keys())
+    assert compressed_keys == uncompressed_keys
+
+    for compressed_attributes, uncompressed_attributes in zip(compressed_body.values(), uncompressed_body.values()):
+        for compressed_attributes, uncompressed_attributes in zip(
+            compressed_attributes.values(), uncompressed_attributes.values()
+        ):
+            for compressed_attribute, uncompressed_attribute in zip(compressed_attributes, uncompressed_attributes):
+                compressed_values = compressed_attribute["value"]
+                uncompressed_values = uncompressed_attribute["value"]
+                for compressed_value, uncompressed_value in zip(compressed_values, uncompressed_values):
+                    compressed_value.pop("_score", None)
+                    uncompressed_value.pop("_score", None)
+
+    assert compressed_body == uncompressed_body
 
     assert compressed_response.num_bytes_downloaded < uncompressed_response.num_bytes_downloaded
