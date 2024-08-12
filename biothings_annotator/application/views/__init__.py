e8ef81f08cb2e345055aa3d95af602bb3e3e2e50
jschaff <jschaff@scripps.edu>
2024-08-12 09:55:39 -0700

    Remove the legacy endpoints

--- biothings_annotator/application/views/__init__.py
+++ biothings_annotator/application/views/__init__.py
@@ -19,12 +19,6 @@ def build_routes() -> list[dict]:
         "uri": r"/curie/<curie:str>",
         "name": "curie_endpoint",
     }
-    curie_route_mirror = {
-        "handler": CurieView.as_view(),
-        "uri": r"/annotator/<curie:str>",
-        "name": "curie_endpoint_mirror",
-    }
-
     batch_curie_route = {
         "handler": BatchCurieView.as_view(),
         "uri": r"/curie/",
@@ -33,13 +27,10 @@ def build_routes() -> list[dict]:
 
     # --- TRAPI ROUTES ---
     trapi_route_main = {"handler": TrapiView.as_view(), "uri": "/trapi/", "name": "trapi_endpoint"}
-    trapi_route_mirror = {"handler": TrapiView.as_view(), "uri": "/annotator/", "name": "trapi_endpoint_mirror"}
 
     route_collection = [
         curie_route_main,
-        curie_route_mirror,
         batch_curie_route,
         trapi_route_main,
-        trapi_route_mirror,
     ]
     return route_collection
