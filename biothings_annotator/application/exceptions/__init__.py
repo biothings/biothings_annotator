e9bf4aaaa54989d049cc1d0165edd8901c00d3ba
jschaff <jschaff@scripps.edu>
2024-08-21 12:57:05 -0700

    Simplify the exception handling process ...
    Removes the definition in place of returning a standard JSON
    response with a 400 return code

--- biothings_annotator/application/exceptions/__init__.py
+++ biothings_annotator/application/exceptions/__init__.py
@@ -4,7 +4,7 @@ Exception handling for the application level
 
 from sanic.exceptions import MethodNotAllowed
 
-from biothings_annotator.application.exceptions.handlers import handle_route_not_found
+from biothings_annotator.application.exceptions.handlers import handle_method_not_allowed
 
 
 def build_exception_handers() -> list[dict]:
@@ -12,6 +12,6 @@ def build_exception_handers() -> list[dict]:
     Builds the collection of dictionary mappings for the
     handler -> exception map
     """
-    method_not_allowed_handler = {"exception": MethodNotAllowed, "handler": handle_route_not_found}
+    method_not_allowed_handler = {"exception": MethodNotAllowed, "handler": handle_method_not_allowed}
     exception_handler_mapping = [method_not_allowed_handler]
     return exception_handler_mapping
--- biothings_annotator/application/exceptions/handlers.py
+++ biothings_annotator/application/exceptions/handlers.py
@@ -2,17 +2,48 @@
 Exception handling for the application level
 """
 
+import logging
+
 import sanic
 from sanic.exceptions import MethodNotAllowed
 from sanic.request import Request
+from sanic import json
 
-from biothings_annotator.application.exceptions.definitions import RoutingHelperException
+logger = logging.getLogger(__name__)
 
 
-async def handle_route_not_found(request: Request, exception: MethodNotAllowed) -> RoutingHelperException:
+async def handle_method_not_allowed(request: Request, exception: MethodNotAllowed) -> json:
     """
     Handler for exceptions in the case where the route / path provided was not
     found by the application
+
+    HTTP 405: MethodNotAllowed exception
     """
-    exception_instance = RoutingHelperException(request=request, exception=exception)
-    return exception_instance
+    method_paths = find_all_paths_by_method(request)
+    status_code = 400
+    error_mapping = {
+        "requestpath": request.path,
+        "message": "Router was unable to find a valid route",
+        "exception": exception.message,
+        "validpaths": method_paths,
+    }
+    error_response = json(error_mapping, status=status_code)
+    return error_response
+
+
+def find_all_paths_by_method(request: Request) -> list[str]:
+    """
+    Finds all similar paths based off the method type
+    """
+    request_method = request.method
+
+    application_context = sanic.Sanic.get_app()
+    router = application_context.router
+
+    similar_method_routes = []
+    for route in router.routes:
+        if request_method in route.methods:
+            similar_method_routes.append(route)
+
+    route_uri = [f"/{route.path}" for route in similar_method_routes]
+    return route_uri
