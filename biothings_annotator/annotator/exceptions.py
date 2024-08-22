2dbb9771404e4971adfd6cf48dae10b54afac399
jschaff <jschaff@scripps.edu>
2024-08-21 20:21:44 -0700

    Update our exception handling and responses ...
    Provides more detailed JSON responses for both /curie/ and
    /trapi/ endpoints. Ensure that we always have a JSON response
    back from our service

--- biothings_annotator/annotator/exceptions.py
+++ biothings_annotator/annotator/exceptions.py
@@ -1,6 +1,18 @@
+from biothings_annotator.annotator.settings import BIOLINK_PREFIX_to_BioThings
+
+
 class TRAPIInputError(ValueError):
-    pass
+    def __init__(self, trapi_input: dict):
+        self.input_structure = trapi_input
+        self.expected_structure = {"message": {"knowledge_graph": {"nodes": {"node0": {}, "node1": {}, "nodeN": {}}}}}
+        self.message = "Unsupported TRAPI input structure"
+        super().__init__()
 
 
 class InvalidCurieError(ValueError):
-    pass
+    def __init__(self, curie: str):
+        self.supported_biolink_nodes = list(BIOLINK_PREFIX_to_BioThings.keys())
+        self.message = f"Unsupported CURIE id: {curie}. "
+        if ":" not in curie:
+            self.message += "Invalid structure for the provided CURIE id. Expected form <node>:<id>"
+        super().__init__()
