e94b86ac5d64a6092ecc9f7bc668f95e11a321a6
jschaff <jschaff@scripps.edu>
2024-09-20 15:29:56 -0700

    Add the HEAD endpoint for the status route

--- biothings_annotator/application/views/annotator.py
+++ biothings_annotator/application/views/annotator.py
@@ -23,6 +23,21 @@ class StatusView(HTTPMethodView):
         cache = application.config.CACHE_MAX_AGE
         self.default_headers = {"Cache-Control": f"max-age={cache}, public"}
 
+    async def head(self, request: Request):
+        curie = "NCBIGene:1017"
+        fields = "_id"
+
+        annotator = Annotator()
+        try:
+            annotated_node = annotator.annotate_curie(curie, fields=fields, raw=False, include_extra=False)
+
+            if "NCBIGene:1017" not in annotated_node:
+                return sanic.json(None, status=500)
+
+            return sanic.json(None, headers=self.default_headers, status=200)
+        except Exception as exc:
+            return sanic.json(None, status=400)
+
     async def get(self, _: Request):
         curie = "NCBIGene:1017"
         fields = "_id"
@@ -33,13 +48,13 @@ class StatusView(HTTPMethodView):
 
             if "NCBIGene:1017" not in annotated_node:
                 result = {"success": False, "error": "Service unavailable due to a failed data check!"}
-                return sanic.json(result)
+                return sanic.json(result, status=500)
 
             result = {"success": True}
-            return sanic.json(result, headers=self.default_headers)
+            return sanic.json(result, headers=self.default_headers, status=200)
         except Exception as exc:
             result = {"success": False, "error": repr(exc)}
-            return sanic.json(result)
+            return sanic.json(result, status=400)
 
 
 class VersionView(HTTPMethodView):
