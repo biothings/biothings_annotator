88870ac3dbfbf0c1a5a2a052a6ca377322ab8a3f
jschaff <jschaff@scripps.edu>
2024-09-25 10:09:32 -0700

    Ensure the __all__ values are strings

--- biothings_annotator/annotator/__init__.py
+++ biothings_annotator/annotator/__init__.py
@@ -4,10 +4,10 @@ from .settings import ANNOTATOR_CLIENTS, BIOLINK_PREFIX_to_BioThings
 from .transformer import ResponseTransformer
 
 __all__ = [
-    Annotator,
-    ResponseTransformer,
-    InvalidCurieError,
-    TRAPIInputError,
-    BIOLINK_PREFIX_to_BioThings,
-    ANNOTATOR_CLIENTS,
+    "Annotator",
+    "ResponseTransformer",
+    "InvalidCurieError",
+    "TRAPIInputError",
+    "BIOLINK_PREFIX_to_BioThings",
+    "ANNOTATOR_CLIENTS",
 ]
