46c6b6fdd3ac5145601c26eb638222c9264b43c5
jschaff <jschaff@scripps.edu>
2024-10-04 15:01:25 -0700

    Add api host test configuration fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,22 @@ fixtures for handling and storing test data for both
 input and output storage
 """
 
+import logging
+import os
+
+import pytest
+
 pytest_plugins = [
     "fixtures.application",
     "fixtures.datastore",
 ]
+
+logger = logging.getLogger(__name__)
+
+
+# ENVIRONMENT CONFIGURATION
+@pytest.fixture(scope="session", autouse=True)
+def annotator_environment():
+    api_host = "https://biothings.test.transltr.io"
+    os.environ["SERVICE_PROVIDER_API_HOST"] = api_host
+    logger.info("Set SERVICE PROVIDER API HOST: %s for tests", api_host)
