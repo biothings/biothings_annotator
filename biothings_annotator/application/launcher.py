8af76a8f1af3beedfeb041ba84d7f805eb8b6618
ctrl-schaff <schaffjohnathan@gmail.com>
2025-12-10 10:20:23 -0600

    Add the swagger ui doc to the launcher

--- biothings_annotator/application/launcher.py
+++ biothings_annotator/application/launcher.py
@@ -9,14 +9,24 @@
 Parallel implementation to our defacto implementation of tornado
 """
 
+import sanic
+import swagger_ui
+
 from biothings_annotator.application.cli.interface import AnnotatorCLI
+from biothings_annotator.application import CONFIGURATION_DIRECTORY
 
 
 def main():
     """
     The entry point for launching the sanic server instance from CLI
     """
-    cli_instance = AnnotatorCLI()
-    cli_instance.attach()
-    cli_instance.parse()
-    cli_instance.run()
+    application: sanic.Sanic = AnnotatorCLI()
+
+    application.attach()
+    application.parse()
+    application.run()
+
+
+def generate_openapi_swagger_ui(application: sanic.Sanic) -> None:
+    openapi_file = CONFIGURATION_DIRECTORY.joinpath("openapi.json")
+    swagger_ui.api_doc(application, config_path=openapi_file, url_prefix="/", title="Annotation UI")
