42d7f3304007b088d319a879dca24baf0a3d5f00
ctrl-schaff <schaffjohnathan@gmail.com>
2025-12-15 07:52:41 -0600

    Revert launcher to original state

--- biothings_annotator/application/launcher.py
+++ biothings_annotator/application/launcher.py
@@ -9,24 +9,14 @@
 Parallel implementation to our defacto implementation of tornado
 """
 
-import sanic
-import swagger_ui
-
 from biothings_annotator.application.cli.interface import AnnotatorCLI
-from biothings_annotator.application import CONFIGURATION_DIRECTORY
 
 
 def main():
     """
     The entry point for launching the sanic server instance from CLI
     """
-    application: sanic.Sanic = AnnotatorCLI()
-
+    application = AnnotatorCLI()
     application.attach()
     application.parse()
     application.run()
-
-
-def generate_openapi_swagger_ui(application: sanic.Sanic) -> None:
-    openapi_file = CONFIGURATION_DIRECTORY.joinpath("openapi.json")
-    swagger_ui.api_doc(application, config_path=openapi_file, url_prefix="/", title="Annotation UI")
